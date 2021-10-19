a2bc9b517f00d0cff5ddad12aae16e44a20be270
Yotam <47275263+YotamZiv298@users.noreply.github.com>
2021-10-19 11:08:06 +0300

    Update transposition_cipher.py

--- transposition_cipher.py
+++ transposition_cipher.py
@@ -1,3 +1,6 @@
+import math
+
+
 def encrypt(key, txt):
     encrypted_txt = [''] * key
 
@@ -9,6 +12,28 @@ def encrypt(key, txt):
             curr_index += key
 
     return ''.join(encrypted_txt)
+
+
+def decrypt(key, txt):
+    num_of_columns = math.ceil(len(txt) / key)
+    num_of_rows = key
+
+    num_of_shaded_boxes = (num_of_columns * num_of_rows) - len(txt)
+
+    plaintext = [''] * num_of_columns
+
+    col = 0
+    row = 0
+
+    for symbol in txt:
+        plaintext[col] += symbol
+        col += 1
+
+        if (col == num_of_columns) or (col == num_of_columns - 1 and row >= num_of_rows - num_of_shaded_boxes):
+            col = 0
+            row += 1
+
+    return ''.join(plaintext)
 
 
 def main():
