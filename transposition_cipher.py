2d1c24eed92acf99a05f9dcab3f0d32b83c2e896
Yotam <47275263+YotamZiv298@users.noreply.github.com>
2021-10-21 13:33:05 +0300

    Update transposition_cipher.py

--- transposition_cipher.py
+++ transposition_cipher.py
@@ -17,7 +17,6 @@ def encrypt(key, txt):
 def decrypt(key, txt):
     num_of_columns = math.ceil(len(txt) / key)
     num_of_rows = key
-
     num_of_shaded_boxes = (num_of_columns * num_of_rows) - len(txt)
 
     plaintext = [''] * num_of_columns
